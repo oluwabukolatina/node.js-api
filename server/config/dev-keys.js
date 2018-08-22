14138e7e7ddd3983985c9b823a626888be99391a
oluwabukolatina <oluwabukolatina2015@gmail.com>
2018-08-22 05:19:47 +0100

    dev keys

---
+++ server/config/dev-keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    mongoURI: 'mongodb://admin:admin11111111@ds149309.mlab.com:49309/events-dev',
+
+   // googleClientID: '721177315518-ca53na0f5gt2aqsvuebq0klhp4qr5sts.apps.googleusercontent.com',
+
+    //googleClientSecret: 'IrMV2Maok-ajoNef9rVM_WGm'
+
+}
